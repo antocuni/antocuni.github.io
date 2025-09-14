cb962d6149f13cb0a801b9c0b4d4a51b984f77c0
Antonio Cuni <anto.cuni@gmail.com>
2025-09-15 00:56:36 +0200

    new demo

---
+++ blog/talk/2025/09/core-dev-sprint-pypy-jit/genpolygons.py
@@ -0,0 +1,30 @@
+"""
+struct Point {
+    double x;
+    double y;
+};
+
+struct Triangle {
+    Point a;
+    Point b;
+    Point c;
+};
+"""
+
+import struct
+import random
+
+def main():
+    N = 300000
+
+    with open('poly.bin', 'wb') as f:
+        for i in range(N):
+            ps = [random.random() for _ in range(6)]
+            fmt = 'dddddd'
+            data = struct.pack(fmt, *ps)
+            f.write(data)
+
+
+
+if __name__ == '__main__':
+    main()
