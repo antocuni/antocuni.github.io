8d7ae38dd09d1ee8fa13806a5c112bd6457a2521
Antonio Cuni <anto.cuni@gmail.com>
2025-11-06 10:29:30 +0100

    some slides

--- blog/talk/template/make-reveal.py
+++ blog/talk/template/make-reveal.py
@@ -19,6 +19,7 @@ HTML_TEMPLATE = """<!DOCTYPE html>
 <head>
     <title>Tracing JIT and real world Python</title>
     <meta name="viewport" content="width=device-width,initial-scale=1">
+    <meta charset="utf-8">
 
     <!-- Reveal.js CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/reveal.js@5.1.0/dist/reveal.min.css">
